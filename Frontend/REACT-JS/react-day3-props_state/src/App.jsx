f05b9ce03e6c1556e6c9a9758f6bc68d9aa983af
Aditya Nath <adityanath271@gmail.com>
2026-07-31 10:51:30 +0530

    React day 3 props and state

---
+++ Frontend/REACT-JS/react-day3-props_state/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const App = () => {
+  let [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Count is - {count} </h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        plus
+      </button>
+    </div>
+  );
+};
+
+export default App;

70c12681359001c11ef5789d7af965a4b7dea4ef
Aditya Nath <adityanath271@gmail.com>
2026-08-01 10:17:26 +0530

    React day 4

---
+++ Frontend/REACT-JS/react-day4-state_data_rendring/src/ProductsCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ProductsCard = ({ product, del }) => {
+  
+  return (
+    <div className="p-2 border-2 flex flex-col gap-4 rounded">
+      <div className="w-40">
+        <img src={product.image} alt="" />
+      </div>
+      <div>
+        <h2 className="font-semibold">{product.title}</h2>
+        <p className="text-xs">{product.category}</p>
+        <p className="text-green-500">{product.price}</p>
+      </div>
+      <button onClick={()=> del(product.id)} className="p-2 bg-red-500">Delete</button>
+    </div>
+  );
+};
+
+export default ProductsCard;

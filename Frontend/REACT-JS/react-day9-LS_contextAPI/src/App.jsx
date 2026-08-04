4af21dc1427b2cbb0110ce12bb9375e97490efd7
Aditya Nath <adityanath271@gmail.com>
2026-08-04 18:42:10 +0530

    made react Local storage

---
+++ Frontend/REACT-JS/react-day9-LS_contextAPI/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Usercard from "./components/Usercard";
+import Form from "./components/Form";
+
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  return (
+    <div className="p-3 h-screen flex flex-col gap-4">
+      <Navbar setToggle={setToggle} />
+
+      {toggle ? (
+        <div className="flex gap-4">
+          {
+            users.map((elem)=>{
+              return <Usercard users={elem}/>
+            })
+          }
+        </div>
+      ) : (
+        <div className=" h-{70%} flex justify-center items-center">
+          <Form setUsers={setUsers} setToggle={setToggle} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ Frontend/REACT-JS/react-day9-LS_contextAPI/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = ({ setToggle }) => {
+  return (
+    <div className="p-4 flex items-center justify-between rounded bg-black">
+      <div>
+        <img
+          className="rounded-full"
+          width={40}
+          src="https://imgs.search.brave.com/lAbYFPtoBU7bCfC5rNYTlG07lp8uBWwRw5BVFqR_nn4/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzFkL2Vj/L2UyLzFkZWNlMmM4/MzU3YmRkN2NlZTNi/MTUwMzYzNDRmYWY1/LmpwZw"
+          alt=""
+        />
+      </div>
+      <div className="flex gap-6 font-semibold">
+        <p>Home</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
+      <button
+        onClick={() => setToggle((prev) => !prev)}
+        className="p-2 bg-blue-700 text-white cursor-pointer"
+      >
+        Create user
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;

ce4eac5b961a21b09688cf34db47f85b815b109d
Aditya Nath <adityanath271@gmail.com>
2026-08-09 17:21:56 +0530

    Local storagein react

---
+++ Frontend/REACT-JS/react-day9-Local_storage/src/components/Usercard.jsx
@@ -0,0 +1,30 @@
+import React, { use } from "react";
+
+const Usercard = ({ users }) => {
+  return (
+    <div className="p-4 border border-white rounded flex flex-col gap-2 bg-black">
+      <div className="h-40 w-50">
+        <img
+          className="object-fit h-full w-full rounded-xl"
+          src={users.image}
+          alt=""
+        />
+      </div>
+      <div className="flex flex-col gap-1">
+        <h1>{users.name}</h1>
+        <p className="text-sm">{users.email}</p>
+        <p className="text-sm">{users.contact}</p>
+      </div>
+      <div className="flex justify-between w-full gap-4">
+        <button className="bg-yellow-700 text-white py-2 px-3 rounded">
+          Update
+        </button>
+        <button className="bg-red-700 text-white py-2 px-3 rounded">
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Usercard;

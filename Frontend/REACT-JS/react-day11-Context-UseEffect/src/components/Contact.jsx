b98678205640879dc6d96e8c65ae4be95d6a8f41
Aditya Nath <adityanath271@gmail.com>
2026-08-12 11:47:33 +0530

    Context API and UseEffect

---
+++ Frontend/REACT-JS/react-day11-Context-UseEffect/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  console.log("Contact Rendring...");
+  return (
+    <div>
+      <h1>Contact page</h1>
+    </div>
+  );
+};
+
+export default Contact;

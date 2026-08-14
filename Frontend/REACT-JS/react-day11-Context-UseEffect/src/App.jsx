c583ff643a173c3b7ab44d35a3fbff25ec761d3c
Aditya Nath <adityanath271@gmail.com>
2026-08-14 11:47:34 +0530

    useEffect first use

--- Frontend/REACT-JS/react-day11-Context-UseEffect/src/App.jsx
+++ Frontend/REACT-JS/react-day11-Context-UseEffect/src/App.jsx
@@ -1,20 +1,25 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import { ContextProvider, MyStore } from "./context/MyContext";
 
 const App = () => {
-  console.log("App Rendring");
+  const [count, setCount] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    console.log("App  Rendring");
+  }, [toggle]);
 
   return (
     <div>
-      <ContextProvider>
-        <Home />
-        <About />
-      </ContextProvider>
-
-      <Contact />
+      <h1>Count is - {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <br />
+      <button onClick={() => setToggle((prev) => !prev)}>
+        Change Toggle state
+      </button>
     </div>
   );
 };

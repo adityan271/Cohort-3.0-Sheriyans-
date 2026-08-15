a3cb326ad8c72d208c532fb96a610e93c02f5a4e
Aditya Nath <adityanath271@gmail.com>
2026-08-15 11:35:07 +0530

    React Router - impliment

--- Frontend/REACT-JS/react-day13-React-router/src/App.jsx
+++ Frontend/REACT-JS/react-day13-React-router/src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import { Route, Routes } from "react-router";
 
 const App = () => {
   return (
     <div className="h-screen p-2">
-      <nav className="flex items-center justify-between" >
+      <nav className="flex items-center justify-between">
         <h1>Logo</h1>
         <div className="flex items-center justify-between gap-10 ">
           <p>Home</p>
@@ -16,9 +17,13 @@ const App = () => {
         <button>Login</button>
       </nav>
 
-      <Home />
-      <About />
-      <Contact />
+      <div>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   );
 };

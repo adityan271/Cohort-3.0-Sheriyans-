b8a9b0d3cccd7f206b2e442d2e5300f529a76717
Aditya Nath <adityanath271@gmail.com>
2026-08-16 21:31:17 +0530

    Dynamic router - protected Route

---
+++ Frontend/REACT-JS/react-day14_DYNAMIC-ROUTER/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navigate } from "react-router";
+
+const ProtectedRoute = ({ children }) => {
+  let isAdmin = false;
+
+  if (!isAdmin) {
+   return <Navigate to={"/"} />;
+  }
+  return children;
+};
+
+export default ProtectedRoute;

497cce708d522884e3834910ea29e260bda0315f
Aditya Nath <adityanath271@gmail.com>
2026-08-20 16:53:14 +0530

    Redux-toolkit used and callled in ui using useDispatch

--- Frontend/REACT-JS/react-day18-Redux_toolkit/src/App.jsx
+++ Frontend/REACT-JS/react-day18-Redux_toolkit/src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrerment, increment } from "./features/counterSlice";
 
 const App = () => {
+  let dispatch = useDispatch();
+  let { count } = useSelector((store) => store.counter);
+
   return (
     <div>
       <h1>Redux - Toolkit</h1>
+      <h1>My count is {count}</h1>
+      <button onClick={() => dispatch(decrerment())}>Decrement</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
     </div>
   );
 };
--- Frontend/REACT-JS/react-day18-Redux_toolkit/src/features/counterSlice.jsx
+++ Frontend/REACT-JS/react-day18-Redux_toolkit/src/features/counterSlice.jsx
@@ -2,23 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const counterSlice = createSlice({
   name: "counter",
-
-  // state for holding data
   initialState: {
     count: 0,
   },
-
-  //actions for updating the state
   reducers: {
     increment: (state) => {
       state.count++;
     },
-    decrement: (state) => {
+    decrerment: (state) => {
       state.count--;
     },
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrerment } = counterSlice.actions;
 
 export default counterSlice.reducer;
